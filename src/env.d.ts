6379fb056c5d81f71cea55337dc5c93cedd9b38d
Luka Golinar <luka.golinar@kalmia.si>
2023-06-26 20:57:30 +0200

    temp: tsconfig revert - build failed before this one.

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,5 +5,7 @@ declare module '*.vue' {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
     const component: DefineComponent<{}, {}, any>;
     export default component;
+    // vite-env.d.ts
+    /// <reference types="vite-plugin-pages/client-solid" />
   }
   
